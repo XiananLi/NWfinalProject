7a742ce5ab1549720924c81aa6cd3b6e5278fd98
Xianan Li <xiananli727@163.com>
2018-05-09 08:29:18 -0700

    Delete replacepic.py

--- replacepic.py
+++
@@ -1,5 +0,0 @@
-def response(flow):
-        if flow.response.headers.get("content-type", "").startswith("image"):
-            img = open("mi.png", "rb").read()
-            flow.response.content = img
-            flow.response.headers["content-type"] = "image/png"
